791d445029738a9d77337428b597cd2887610b97
Damon <dabIT@hampshire.edu>
2014-12-19 16:34:35 -0500

    Trying to get these physics working correctly.

---
+++ Unity/Assets/BoxDrop.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxDrop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -10.0f) {
+			// reset physics stuff
+			gameObject.rigidbody.velocity = Vector3.zero;
+			gameObject.rigidbody.angularVelocity = Vector3.zero;
+
+			// reset psotion and rotation
+			transform.rotation = Quaternion.identity;
+			transform.position = new Vector3(0.0f, 10.0f, 0.0f);
+		}
+	}
+}
