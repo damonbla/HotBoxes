18478e546002532046c55d861cee99802a9fb81b
Damon <dabIT@hampshire.edu>
2014-12-25 17:20:01 -0500

    Star Cube beginnings.

---
+++ Unity/Assets/Scripts/StarCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarCube : MonoBehaviour {
+
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotateAmount = rotateSpeed * Time.deltaTime;
+
+		transform.Rotate(new Vector3(rotateAmount, -rotateAmount, rotateAmount * 0.5f));
+	}
+}
