1c8e17afee72dd35369a0b00a16454b8906f9b7b
Damon <dabIT@hampshire.edu>
2014-12-25 19:03:32 -0500

    Background cubes moving randomly and wavy-like!

---
+++ Unity/Assets/Scripts/BackgroundCubeMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundCubeMove : MonoBehaviour {
+
+	private float startZ;
+	private float timeAdjustment;
+
+	// Use this for initialization
+	void Start () {
+		startZ = Random.Range(-10, 10) * 0.05f;
+		timeAdjustment = Random.Range(-10, 10) * 0.3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x, transform.position.y, 10.0f - startZ * Mathf.Sin(Time.time + timeAdjustment));
+	}
+}
--- Unity/Assets/Scripts/GameplayScript.cs
+++ Unity/Assets/Scripts/GameplayScript.cs
@@ -12,13 +12,13 @@ public class GameplayScript : MonoBehaviour {
 		// instantiate all the background cubes
 		for (int x = 0; x < 7; x++) {
 			for (int y = 0; y < 10; y++) {
-				backgroundCubes[x,y] = (GameObject)Instantiate(backgroundCube, new Vector3((x - 3.0f) * 2.0f, (y - 4.0f) * 2.0f, 5.0f), Quaternion.identity);
+				backgroundCubes[x,y] = (GameObject)Instantiate(backgroundCube, new Vector3((x - 3.0f) * 2.0f, (y - 4.0f) * 2.0f, 10.0f), Quaternion.identity);
 			}
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
