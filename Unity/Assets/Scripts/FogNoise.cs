456b553bfa6d58dad5f4ea48d050535b07f34867
Damon <dabIT@hampshire.edu>
2015-01-24 16:27:06 -0500

    Some perlin fog going.

---
+++ Unity/Assets/Scripts/FogNoise.cs
@@ -0,0 +1,39 @@
+﻿// this script is from http://docs.unity3d.com/ScriptReference/Mathf.PerlinNoise.html
+// edited for my own needs
+
+using UnityEngine;
+using System.Collections;
+
+public class FogNoise : MonoBehaviour {
+    public int pixWidth;
+    public int pixHeight;
+    public float xOrg;
+    public float yOrg;
+    public float scale = 1.0F;
+    private Texture2D noiseTex;
+    private Color[] pix;
+    void Start() {
+        noiseTex = new Texture2D(pixWidth, pixHeight);
+        pix = new Color[noiseTex.width * noiseTex.height];
+        renderer.material.mainTexture = noiseTex;
+    }
+    void CalcNoise() {
+        float y = 0.0F;
+        while (y < noiseTex.height) {
+            float x = 0.0F;
+            while (x < noiseTex.width) {
+                float xCoord = xOrg + x / noiseTex.width * scale + Time.time * 0.3f;
+                float yCoord = yOrg + y / noiseTex.height * scale - Time.time * 0.3f;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                pix[(int)y * noiseTex.width + (int)x] = new Color(sample - 0.3f, sample, sample - 0.2f, sample);
+                x++;
+            }
+            y++;
+        }
+        noiseTex.SetPixels(pix);
+        noiseTex.Apply();
+    }
+    void Update() {
+        CalcNoise();
+    }
+}
