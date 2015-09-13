1474a2ea7eaa7be832f7559ce77c9ff566a8f154
damonbla <damonbla@gmail.com>
2015-09-13 15:34:47 -0400

    Indicator arrow working great.

--- Unity/Assets/Scripts/GameplayScript.cs
+++ Unity/Assets/Scripts/GameplayScript.cs
@@ -10,8 +10,15 @@ public class GameplayScript : MonoBehaviour {
 	// indicator arrows for when dropping cube goes above the top of the screen
 	public GameObject indicatorArrow;
 	private GameObject indicatorParent;
+	private bool indicatorActive;
 
 	private VariableControl variables;
+
+
+
+	//------------------------------------- Helper functions --------------------------------------
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -30,21 +37,33 @@ public class GameplayScript : MonoBehaviour {
 			}
 		}
 
+		// indicator for when the falling cube is above the top of the screen
+		// its parent that is
 		indicatorParent = new GameObject();
-		Vector3 parentPos = new Vector3(0.0f, 0.0f, 0.0f);
+		Vector3 indicatorParentPos = new Vector3(0.0f, 0.0f, 0.0f);
+		indicatorParent.transform.position = indicatorParentPos;		
 
-		indicatorParent.transform.position = parentPos;
 		indicatorArrow = (GameObject)Instantiate (indicatorArrow, new Vector3 (0.0f, 0.0f, 0.0f), Quaternion.identity);
-
 		indicatorArrow.transform.parent = indicatorParent.transform;
+		indicatorArrow.SetActive(false);
+		indicatorActive = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// get the indicator arrows on the screen
-		// need to get them in the right place: based on the x value of the dropping cube
-		float dropX = GameObject.Find ("Dropping Box").transform.position.x;
-		indicatorParent.transform.position = new Vector3 (dropX, 6.8f, 0.0f);
+		// need them to be in the right place: based on the x value of the dropping cube
+		if (GameObject.Find ("Dropping Box").transform.position.y > 8.0f) {
+			if (!indicatorActive) {
+				indicatorArrow.SetActive(true);
+			}
+			float dropX = GameObject.Find ("Dropping Box").transform.position.x;
+			indicatorParent.transform.position = new Vector3 (dropX, 6.8f, 0.0f);
+		} else {
+			indicatorParent.transform.position = new Vector3 (0.0f, 00.0f, 0.0f);
+			indicatorArrow.SetActive(false);
+			indicatorActive = false;
+		}
 	}
 
 }
